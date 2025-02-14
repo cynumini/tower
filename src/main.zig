6ca57b6b0873dcd815934d759f35087ba2a13e0d
cynumini <cynumini@proton.me>
2025-02-14 17:34:40 +0200

    add basic movement

--- src/main.zig
+++ src/main.zig
@@ -1,26 +1,77 @@
 const std = @import("std");
 const c = @cImport({
     @cInclude("raylib.h");
+    @cInclude("raymath.h");
 });
 
 pub fn main() !void {
-    const screenWidth = 800;
-    const screenHeight = 450;
+    var screen_width: i32 = 640;
+    var screen_height: i32 = 360;
+    var player_position = c.Vector2Zero();
 
-    c.InitWindow(screenWidth, screenHeight, "tower");
+    c.InitWindow(screen_width, screen_height, "tower");
 
     c.SetTargetFPS(60);
 
+    var camera: c.Camera2D = .{
+        .zoom = 1,
+        .offset = .{ .x = 640 / 2, .y = 360 / 2 },
+    };
+
     while (!c.WindowShouldClose()) {
         // Update
+        if (c.IsWindowResized()) {
+            screen_width = c.GetScreenWidth();
+            screen_height = c.GetScreenHeight();
+            camera = .{
+                .zoom = @as(f32, @floatFromInt(screen_height)) / 360,
+                .offset = .{
+                    .x = @floatFromInt(@divExact(screen_width, 2)),
+                    .y = @floatFromInt(@divExact(screen_height, 2)),
+                },
+            };
+        }
+        var player_move = c.Vector2Zero();
+        if (c.IsKeyDown(c.KEY_RIGHT)) player_move.x += 1;
+        if (c.IsKeyDown(c.KEY_LEFT)) player_move.x -= 1;
+        if (c.IsKeyDown(c.KEY_UP)) player_move.y -= 1;
+        if (c.IsKeyDown(c.KEY_DOWN)) player_move.y += 1;
+
+        player_move = c.Vector2Scale(c.Vector2Normalize(player_move), 4);
+        player_move.y /= 2;
+        player_position = c.Vector2Add(player_position, player_move);
+
+        camera.target = player_position;
 
         // Draw
         c.BeginDrawing();
 
         c.ClearBackground(c.RAYWHITE);
 
-        c.DrawText("Congrats! You created your first window!", 190, 200, 20, c.LIGHTGRAY);
+        c.BeginMode2D(camera);
 
+        c.DrawText(
+            "Congrats! You created your first window!",
+            190,
+            200,
+            20,
+            c.LIGHTGRAY,
+        );
+        c.DrawEllipse(
+            0,
+            0,
+            @divExact(640, 2),
+            @divExact(640, 4),
+            c.RED,
+        );
+
+        c.DrawRectangleV(
+            c.Vector2{ .x = player_position.x - 16, .y = player_position.y - 16 },
+            c.Vector2{ .x = 32, .y = 32 },
+            c.BLUE,
+        );
+
+        c.EndMode2D();
         c.EndDrawing();
     }
 
